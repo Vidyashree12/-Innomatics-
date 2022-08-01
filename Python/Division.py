5e632052c5b67e4ccd0198c39e57a6a93f4637c1
Vidya Shree Nayak <78997278+Vidyashree12@users.noreply.github.com>
2022-08-01 12:41:51 +0530

    Add files via upload

---
+++ Python/Division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+a = int(input())
+b = int(input())
+print(a//b)
+print(a/b)
+
---
+++ Python/Loops.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+n = int(input())
+   for i in range(n):
+       print(i**2)
+
