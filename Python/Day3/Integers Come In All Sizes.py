12b03c085f789598bbceb9fc3ba2439d3e812b21
Vidya Shree Nayak <78997278+Vidyashree12@users.noreply.github.com>
2022-08-01 23:22:28 +0530

    Add files via upload

---
+++ Python/Day3/Integers Come In All Sizes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a,b,c,d = (int(input()) for _ in range(4))
+print(pow(a,b)+pow(c,d))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ Python/Day3/Power - Mod Power.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a,b,m=(int(input()) for _ in range(3))
+
+print(pow(a,b))
+print(pow(a,b,m))
+
+
+# In[ ]:
+
+
+
+
